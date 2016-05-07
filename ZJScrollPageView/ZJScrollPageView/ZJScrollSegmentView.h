d195f522147a7e75a28248ced1f524d80104fbb6
ZeroJ <854136959@qq.com>
2016-05-07 18:48:35 +0800

    添加部分注释

--- ZJScrollPageView/ZJScrollPageView/ZJScrollSegmentView.h
+++ ZJScrollPageView/ZJScrollPageView/ZJScrollSegmentView.h
@@ -21,10 +21,15 @@ typedef void(^ExtraBtnOnClick)(UIButton *extraBtn);
 @property (strong, nonatomic) UIImage *backgroundImage;
 
 - (instancetype)initWithFrame:(CGRect )frame segmentStyle:(ZJSegmentStyle *)segmentStyle titles:(NSArray *)titles titleDidClick:(TitleBtnOnClickBlock)titleDidClick;
+/** 点击按钮的时候调整UI*/
 - (void)adjustUIWhenBtnOnClickWithAnimate:(BOOL)animated;
+/** 切换下标的时候根据progress同步设置UI*/
 - (void)adjustUIWithProgress:(CGFloat)progress oldIndex:(NSInteger)oldIndex currentIndex:(NSInteger)currentIndex;
+/** 让选中的标题居中*/
 - (void)adjustTitleOffSetToCurrentIndex:(NSInteger)currentIndex;
+/** 设置选中的下标*/
 - (void)setSelectedIndex:(NSInteger)index animated:(BOOL)animated;
+/** 重新刷新标题的内容*/
 - (void)reloadTitlesWithNewTitles:(NSArray *)titles;
 
 @end
--- ZJScrollPageView/ZJScrollPageView/ZJSegmentStyle.h
+++ ZJScrollPageView/ZJScrollPageView/ZJSegmentStyle.h
@@ -8,30 +8,41 @@
 
 #import <UIKit/UIKit.h>
 @interface ZJSegmentStyle : NSObject
-
+/** 是否显示遮盖 默认为NO */
 @property (assign, nonatomic, getter=isShowCover) BOOL showCover;
+/** 是否显示滚动条 默认为NO*/
 @property (assign, nonatomic, getter=isShowLine) BOOL showLine;
+/** 是否缩放标题 默认为NO*/
 @property (assign, nonatomic, getter=isScaleTitle) BOOL scaleTitle;
+/** 是否滚动标题 默认为YES 设置为NO的时候所有的标题将不会滚动, 和系统的segment效果相似 */
 @property (assign, nonatomic, getter=isScrollTitle) BOOL scrollTitle;
+/** 是否颜色渐变 默认为NO*/
 @property (assign, nonatomic, getter=isGradualChangeTitleColor) BOOL gradualChangeTitleColor;
+/** 是否显示附加的按钮 默认为NO*/
 @property (assign, nonatomic, getter=isShowExtraButton) BOOL showExtraButton;
-
+/** 设置附加按钮的背景图片 默认为nil*/
 @property (strong, nonatomic) NSString *extraBtnBackgroundImageName;
-
+/** 滚动条的高度 默认为2 */
 @property (assign, nonatomic) CGFloat scrollLineHeight;
+/** 滚动条的颜色 */
 @property (strong, nonatomic) UIColor *scrollLineColor;
-
+/** 遮盖的颜色 */
 @property (strong, nonatomic) UIColor *coverBackgroundColor;
+/** 遮盖的圆角 默认为14*/
 @property (assign, nonatomic) CGFloat coverCornerRadius;
+/** 遮盖的高度 默认为28*/
 @property (assign, nonatomic) CGFloat coverHeight;
-
+/** 标题之间的间隙 默认为15.0 */
 @property (assign, nonatomic) CGFloat titleMargin;
+/** 标题的字体 默认为14 */
 @property (strong, nonatomic) UIFont *titleFont;
+/** 标题缩放倍数, 默认1.3 */
 @property (assign, nonatomic) CGFloat titleBigScale;
-
+/** 标题一般状态的颜色 */
 @property (strong, nonatomic) UIColor *normalTitleColor;
+/** 标题选中状态的颜色 */
 @property (strong, nonatomic) UIColor *selectedTitleColor;
-
+/** segmentVIew的高度, 这个属性只在使用ZJScrollPageVIew的时候设置生效 */
 @property (assign, nonatomic) CGFloat segmentHeight;
 
 
